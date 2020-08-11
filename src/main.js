6211f985009a3e634613ad0527b6beab8f57d73a
FutureMeng <futuremeng@gmail.com>
2020-08-11 16:15:42 +0800

    去掉electron-store

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 /*
  * @Date: 2020-08-07 08:39:00
  * @LastEditors: Future Meng
- * @LastEditTime: 2020-08-07 16:21:55
+ * @LastEditTime: 2020-08-11 16:08:39
  */
 import Vue from "vue";
 import App from "./App.vue";
@@ -10,8 +10,7 @@ import router from "./router";
 import store from "./store";
 import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
-import inject from "./plugins/inject";
-Vue.use(inject);
+
 Vue.use(ElementUI, { size: "small", zIndex: 3000 });
 Vue.config.productionTip = false;
 
