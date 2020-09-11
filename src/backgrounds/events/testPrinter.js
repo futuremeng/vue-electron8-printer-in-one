fcc87776aff2ab444051c3c1442682a17192d866
FutureMeng <futuremeng@gmail.com>
2020-09-11 13:23:14 +0800

    搞定中文预编码后用buffer传输

--- src/backgrounds/events/testPrinter.js
+++ src/backgrounds/events/testPrinter.js
@@ -1,7 +1,7 @@
 /*
  * @Date: 2020-09-08 16:42:24
  * @LastEditors: Future Meng
- * @LastEditTime: 2020-09-10 17:20:51
+ * @LastEditTime: 2020-09-11 13:22:53
  */
 const printer = require("@thiagoelg/node-printer");
 
@@ -13,22 +13,34 @@ export default function testPrinter(event) {
   // console.log('default printer name: ' + (printer.getDefaultPrinterName() || 'is not defined on your computer'));
   var command = tsc.jpPrinter.createNew();
   console.log(command);
-  // command.setCodepage("65001");
   command.setSize(60, 40);
   command.setGap(2);
   command.setCls();
   command.setText(50, 10, "2", 1, 1, "Hello");
-  command.setText(50, 100, "TSS24.BF2", 1, 1, "\u8fd9\u662fthis is a example");
+  command.setText(50, 100, "TSS24.BF2", 1, 1, "一二三");
   // command.setQR(50, 50, "L", 5, "A", "977767937@qq.com");
   command.setPagePrint();
 
-  // let data = command.getData();
-  // let data = 'TEXT 50,50,"4",0,1,1,"DEMO FOR TEXT"';
-  // let data = "Test";
-  let data = command.getData();
-  let jobid = "";
+  var uint8Buf = command.getData();
+  //下面这种来自于uni的版本，大概不依赖nodejs吧，在nodejs环境下不行。
+  // var data = Array.from(uint8Buf);
+  // var buffer = new ArrayBuffer(data.length);
+  
+  // var dataView = new DataView(buffer);
+  // for (var i = 0; i < data.length; ++i) {
+  //   dataView.setUint8(i, data[i]);
+  // }
+  // console.log(
+  //   "data type is: " + typeof data + ", is buffer: " + Buffer.isBuffer(data)
+  // );
+  
+  var buffer=Buffer.from(uint8Buf);
+  console.log(
+    "buffer type is: " + typeof buffer + ", is buffer: " + Buffer.isBuffer(buffer)
+  );
+  var jobid = "";
   printer.printDirect({
-    data: data, // or simple String: "some text"
+    data: buffer, // or simple String: "some text"
     printer: "Deli_DL_888B_NEW_", // printer name, if missing then will print to default printer
     type: "RAW", // type: RAW, TEXT, PDF, JPEG, COMMAND.. depends on platform
     success: function(jobID) {
